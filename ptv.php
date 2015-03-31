b89473b6a9f50465b86033d40572cdf67982e92b
Rio Bahtiar <akhibahtiar@gmail.com>
2015-04-01 01:22:08 +0700

    My Personal Site begin here

---
+++ ptv.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<title>Rio Bahtiar Portofolio</title>
+<meta name="description" content="Daftar Portofolio Rio Bahtiar">
+<meta name="keywords" content="rio bahtiar, web developer,portofolio">
+<meta name="author" content="+RioBahtiar">
+<link href="/rio.my.id/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="/rio.my.id/assets/css/gaya_home.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="container-fluid">
+</div>
+
+<script src="/rio.my.id/assets/js/jquery-1.11.1.min.js"></script>
+<script src="/rio.my.id/assets/js/lightbox.min.js"></script>
+<script src="/rio.my.id/assets/js/bootstrap.min.js"></script>
+<script src="/rio.my.id/assets/js/jquery.smooth-scroll.js"></script>
+</body>
+</html>
